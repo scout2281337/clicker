a735b3521d646bec5141c4aee39ef529edc7706f
boberHelicopter <146185803+scout2281337@users.noreply.github.com>
2025-06-25 12:53:22 +0300

    1.5

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,21 +9,28 @@ public class GameManager : MonoBehaviour
     public float score;
     public float TotalScoreToWin;
     [SerializeField] private GameObject VictoryScreen;
+
+    [SerializeField]private List<TextMeshProUGUI> inGameScoreTexts;
+
     public void AddScore(float Amount) 
     {
         score += Amount;
-    }
-
-    private void Update()
-    {
-        if (score > TotalScoreToWin) 
+        UpdateScore();
+        if (score > TotalScoreToWin)
         {
             VictoryScreen.SetActive(true);
         }
     }
-
     public void StartNewGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void UpdateScore() 
+    {
+        foreach (var txt in inGameScoreTexts) 
+        {
+            txt.text = $"Score: {score}";
+        }
     }
 }
